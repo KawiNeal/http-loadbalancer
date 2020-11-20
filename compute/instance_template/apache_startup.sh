19725ce2581f479b50a0e249b67107d333e489b4
KLNealDev <kawi.neal@gmail.com>
2020-11-20 13:06:19 -0500

    apache startup update

--- compute/instance_template/apache_startup.sh
+++ compute/instance_template/apache_startup.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-apt-get update 
-apt-get install -y apache2 php
-apt-get install -y wget
+sudo apt-get update 
+sudo apt-get install -y apache2 php
+sudo apt-get install -y wget
 cd /var/www/html
-rm index.html -f
-rm index.php -f
-wget https://storage.googleapis.com/cloud-training/gcpnet/httplb/index.php
+sudo rm index.html -f
+sudo rm index.php -f
+sudo wget https://raw.githubusercontent.com/KawiNeal/http-loadbalancer/master/compute/instance_template/index.php
 META_REGION_STRING=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/zone" -H "Metadata-Flavor: Google")
 REGION=`echo "$META_REGION_STRING" | awk -F/ '{print $4}'`
-sed -i "s|region-here|$REGION|" index.php
+sudo sed -i "s|region-here|$REGION|" index.php
